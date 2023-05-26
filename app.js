e12fd4b96f62cfb5efd410cd4f5d7b1ed38dccc2
NathaciaLais <nathacialais@gmail.com>
2023-05-26 10:43:10 +0700

    connect

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+require('dotenv/config')
 
 app.get("/", (req, res) => {
   res.send("Hello World3!");
-});
+})
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Server running on port ${process.env.PORT}`);
+})
